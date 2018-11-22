6fd30dbca1b23e57dfe873542b1d6984f301820f
Rio Yudha P <rio.pahlevi65@yahoo.com>
2018-11-22 08:37:57 +0700

    master product crud backend

---
+++ bisnislogics/M_Product_Logic.js
@@ -0,0 +1,81 @@
+const ResponseHelper = require("../helpers/Response_Helper");
+const dtl = require("../datalayers/M_Product_Data");
+
+const M_Product_Data = {
+    //GET PRODUCT TABLE
+    readAllHandler: (req, res, next) => {
+        //console.log("disini");
+        dtl.readAllHandlerData(items => {
+          ResponseHelper.sendResponse(res, 200, items);
+          //console.log(JSON.stringify(items))
+        });
+    },
+
+    //GET PRODUCT BY ID
+    readByIdHandler: (req, res, next) => {
+        let id = req.params.productId;
+        dtl.readByIdHandler(items => {
+          ResponseHelper.sendResponse(res, 200, items);
+        }, id);
+      },
+
+    //ADD PRODUCT TABLE
+    createHandler: (req, res, next) => {
+        dtl.readLastId(companies => {
+          if (companies.length > 0) {
+            let pattern = companies[0].code.substr(-4);
+            let lastestCode = parseInt(pattern) + 1;
+            let generatePattern = pattern.substr(
+              0,
+              pattern.length - lastestCode.toString().length
+            );
+            var newCode = "PR" + generatePattern + lastestCode;
+          } else {
+            var newCode = "PR0001";
+          }
+    
+          const data = {
+            code: newCode,
+            name: req.body.name,
+            description: req.body.description,
+            is_delete: false,
+            created_by: req.body.created_by,
+            created_date: new Date().toDateString()
+          }
+    
+          dtl.createHandler(function(items) {
+            ResponseHelper.sendResponse(res, 200, items);
+          }, data);
+        });
+    },
+
+    //DELETE PRODUCT
+    deleteHandler: (req, res, next) => {
+        let id = req.params.productId;
+        dtl.deleteHandler(items => {
+          ResponseHelper.sendResponse(res, 200, items);
+        }, id);
+    },
+
+    //EDIT PRODUCT
+    updateHandler: (req, res, next) => {
+        console.log(req.body);
+        let id = req.params.productId;
+        const data = {
+          name: req.body.name,
+          description: req.body.description,
+          update_date: new Date().toDateString(),
+          update_by : req.body.update_by
+          //update_by: ""
+        };
+        dtl.updateHandler(
+          items => {
+            ResponseHelper.sendResponse(res, 200, items);
+          },
+          data,
+          id
+        );
+      },
+}
+
+module.exports = M_Product_Data
--- routes/Route.js
+++ routes/Route.js
@@ -1,10 +1,25 @@
 const authenticate = require("../helpers/Auth_Helper");
-const souvenirLogic = require("../bisnislogics/M_Souvenir_Logic");
+const productLogic = require("../bisnislogics/M_Product_Logic");
+const t_souvernir_Logic = require("../bisnislogics/T_Souvernir_Logic");
 
 module.exports = server => {
   // Root Route
   server.get("/", (req, res, next) => {});
 
-  // All Routes Here
+  // Product Route
+  // Made By: Rio Yudha P
+  server.get("/api/product", productLogic.readAllHandler);
+  server.get("/api/product/:productId", productLogic.readByIdHandler);
+  server.post("/api/product", productLogic.createHandler);
+  server.put("/api/product/:productId",productLogic.updateHandler);
+  server.del("/api/product/:productId",productLogic.deleteHandler);
+  //== End of Product Route
 
+  // Transaksi Souvernir
+  server.get("/api/tsouvernir", t_souvernir_Logic.readSouvernirAllHandler);
+  //server.get("/api/tsouvernir", t_souvernir_Logic.readSouvernirAllHandler);
+  server.post("/api/tsouvernir", t_souvernir_Logic.createHandler);
+  
+
+  //==End of Transaksi Souvernir
 };
