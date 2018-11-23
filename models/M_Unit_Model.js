72be0fea18edf18483b635e67f8469252b21bb63
unknown <150396fa.,714muztreforjohnsonalveiro714150396FAMOUSLASTWORLD>
2018-11-23 13:47:07 +0700

    modified:   models/M_Unit_Model.js || Ganti Variabel

--- models/M_Unit_Model.js
+++ models/M_Unit_Model.js
@@ -4,11 +4,11 @@ this._id=unit_data._id
 this.code=unit_data.code
 this.name=unit_data.name
 this.description=unit_data.description
-this.isDelete=unit_data.is_delete
-this.createdBy=unit_data.created_by
-this.createdDate=unit_data.created_date
-this.updatedBy=unit_data.updated_by
-this.updatedDate=unit_data.updated_date
+this.is_delete=unit_data.is_delete
+this.created_by=unit_data.created_by
+this.created_date=unit_data.created_date
+this.updated_by=unit_data.updated_by
+this.updated_date=unit_data.updated_date
 
 }
 M_Unit.prototype.getData=function(){
@@ -17,11 +17,11 @@ M_Unit.prototype.getData=function(){
         code :this.code,
         name:this.name,
         description :this.description,
-        isDelete :this.isDelete,
-        createdBy   :this.createdBy,
-        createdDate  :this.createdDate,
-        updatedBy :this.updatedBy,
-        updatedDate:this.updatedDate,
+        is_delete :this.isDelete,
+        created_by   :this.createdBy,
+        created_date  :this.createdDate,
+        updated_by :this.updatedBy,
+        updated_date:this.updatedDate,
     }
 }
 
