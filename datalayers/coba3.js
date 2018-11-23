f534c2e8d31ee094d4ade54380a8a03636fb04b1
yuanda26 <yuanda.dian@gmail.com>
2018-11-23 09:58:03 +0700

    Remove Bug & Unecessary Files

--- datalayers/coba3.js
+++
@@ -1,6 +0,0 @@
-const func = (queryData)=>{
-    return queryData.map(content=>{
-        return {"$and":[{"m_role_id": "RO0001"}, {"m_menu_id": content}]}
-    });
-}
-console.log(JSON.stringify(func(["lele", "lulu"])))
